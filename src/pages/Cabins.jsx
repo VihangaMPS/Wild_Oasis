88fc38f0fa42e229f47c21e5555b720094fa18b1
MPS Vihanga <>
2024-04-30 07:00:28 +0530

    deleting a cabin

---
+++ src/pages/Cabins.jsx
@@ -0,0 +1,23 @@
+import Heading from "../ui/Heading";
+import Row from "../ui/Row";
+import CabinTable from "../features/cabins/CabinTable.jsx";
+
+function Cabins() {
+
+
+    return (
+        <>
+            <Row type="horizontal">
+                <Heading as="h1">All cabins</Heading>
+                <p>TEST</p>
+
+            </Row>
+
+            <Row>
+                <CabinTable/>
+            </Row>
+        </>
+    );
+}
+
+export default Cabins;
