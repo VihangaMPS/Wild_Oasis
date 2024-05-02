68fb5c390bc97979468c475b28ce7bdd14c7f894
MPS Vihanga <>
2024-05-02 18:53:55 +0530

    creatCabins page updated

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,9 +1,12 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable.jsx";
+import Button from "../ui/Button.jsx";
+import {useState} from "react";
+import CreateCabinForm from "../features/cabins/CreateCabinForm.jsx";
 
 function Cabins() {
-
+    const [showForm, setShowForm] = useState(false);
 
     return (
         <>
@@ -15,6 +18,9 @@ function Cabins() {
 
             <Row>
                 <CabinTable/>
+
+                <Button onClick={() => setShowForm(show => !show)}>Add new cabin</Button>
+                {showForm && <CreateCabinForm/>}
             </Row>
         </>
     );
