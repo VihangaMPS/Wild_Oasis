dd700e8664d8ebfbacc4bb3ee7171201cd29d95a
MPS Vihanga <>
2024-05-19 16:21:45 +0530

    error & restart again in react form part

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://lllujxerdnxmekqmajlz.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxsbHVqeGVyZG54bWVrcW1hamx6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM2MTkyNjcsImV4cCI6MjAyOTE5NTI2N30.SslORJX8eGe-hzTv9GVkY_ti8aUWPMhmuoeIdJ0OwHQ';
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
