df0521cdaee32d179b36b328d01085600f110ab0
MPS Vihanga <>
2024-05-19 18:57:00 +0530

    creating a new cabin

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -27,3 +27,19 @@ export async function deleteCabins(id) {
     return data;
 
 }
+
+export async function createCabin(newCabin) {
+
+    const { data, error } = await supabase
+        .from('cabins')
+        .insert([newCabin])
+        .select()
+
+    if (error) {
+        console.log(error);
+        throw new Error('Cabins could not be Created!');
+    };
+
+    return data;
+
+}
