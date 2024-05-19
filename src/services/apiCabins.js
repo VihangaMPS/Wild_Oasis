29f73e660bdc8061e64f85eff88f2aad48024356
MPS Vihanga <>
2024-05-19 23:54:53 +0530

    uploading a image to supabase

---
+++ src/services/apiCabins.js
@@ -0,0 +1,65 @@
+import supabase, {supabaseUrl} from "./supabase.js";
+
+export async function getCabins() {
+
+    const { data, error } = await supabase.from('cabins').select('*');
+
+    if (error) {
+        console.log(error);
+        throw new Error('Cabins could not be loaded!');
+    }
+
+    return data;
+}
+
+export async function deleteCabins(id) {
+
+    const { data, error } = await supabase
+        .from('cabins')
+        .delete()
+        .eq('id', id);
+
+    if (error) {
+        console.log(error);
+        throw new Error('Cabins could not be deleted!');
+    };
+
+    return data;
+
+}
+
+export async function createCabin(newCabin) {
+
+    const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll("/","");
+    const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
+    // 1. Create cabin
+    const { data, error } = await supabase
+        .from('cabins')
+        .insert([{...newCabin, image: imagePath}])
+        .select()
+
+    if (error) {
+        console.log(error);
+        throw new Error('Cabins could not be Created!');
+    };
+
+    // 2. Upload image
+    const { error:storageError } = await supabase
+        .storage
+        .from('cabin-images')
+        .upload(imageName, newCabin.image)
+
+    // 3. Delete Cabin if there was an error uploading image
+    if (storageError) {
+        await supabase.from("cabins").delete().eq("id", data.id);
+
+        console.error(storageError);
+        throw new Error(
+            "Cabin image could not be uploaded and the cabin was not created"
+        );
+    }
+
+    return data;
+
+}
