fa71608063e47526e3fcdc593181670a55ac0ec5
MPS Vihanga <>
2024-07-03 12:50:04 +0530

    API-Side Sorting Bookings

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -5,8 +5,11 @@ export async function getBookings({filter, sortBy}) {
   let query =  supabase.from('bookings')
       .select("id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(fullName,email)");
 
-  // Filter
-  if (filter !== null ) query = query[filter.method || "eq"](filter.field, filter.value);
+  // Filtering
+  if (filter) query = query[filter.method || "eq"](filter.field, filter.value);
+
+  // Sorting
+  if (sortBy) query = query.order(sortBy.field, {ascending: sortBy.direction === "asc"});
 
   const {data, error} = await query;
 
