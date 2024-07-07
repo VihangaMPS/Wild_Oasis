a1000e4d431b598708b3c76dc6eae8ec1e1877f5
MPS Vihanga <>
2024-07-07 19:52:02 +0530

    Authentication User login with Supabase

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -1,41 +1,52 @@
-import { useState } from "react";
+import {useState} from "react";
 import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
+import {useLogin} from "./useLogin.js";
+import SpinnerMini from "../../ui/SpinnerMini.jsx";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("vihanga@example.com");
+    const [password, setPassword] = useState("pw12345");
+    const {login, isLoading} = useLogin();
 
-  function handleSubmit() {}
+    function handleSubmit(e) {
+        e.preventDefault();
 
-  return (
-    <Form onSubmit={handleSubmit}>
-      <FormRow label="Email address" orientation="vertical">
-        <Input
-          type="email"
-          id="email"
-          // This makes this form better for password managers
-          autoComplete="username"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </FormRow>
-      <FormRow label="Password" orientation="vertical">
-        <Input
-          type="password"
-          id="password"
-          autoComplete="current-password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </FormRow>
-      <FormRow orientation="vertical">
-        <Button size="large">Login</Button>
-      </FormRow>
-    </Form>
-  );
+        if (!email || !password) return;
+
+        login({email, password});
+    }
+
+    return (
+        <Form onSubmit={handleSubmit}>
+            <FormRow label="Email address" orientation="vertical">
+                <Input
+                    type="email"
+                    id="email"
+                    // This makes this form better for password managers
+                    autoComplete="username"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    disabled={isLoading}
+                />
+            </FormRow>
+            <FormRow label="Password" orientation="vertical">
+                <Input
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    disabled={isLoading}
+                />
+            </FormRow>
+            <FormRow orientation="vertical">
+                <Button size="large" disabled={isLoading}>{!isLoading ? 'Login' : <SpinnerMini/>}</Button>
+            </FormRow>
+        </Form>
+    );
 }
 
 export default LoginForm;
---
+++ src/services/apiAuth.js
@@ -0,0 +1,13 @@
+import supabase from "./supabase.js";
+
+export async function login({email, password}) {
+
+    let { data, error } = await supabase.auth.signInWithPassword({
+        email: email,
+        password: password
+    })
+
+    if (error) throw new Error(error.message);
+
+    return data;
+}
